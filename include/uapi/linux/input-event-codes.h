6a62775e95b8b918e6cbc75df71777488bf385b4
LinkBoi00 <linkdevel@protonmail.com>
2022-12-31 00:46:55 +0200

    drivers: misc/mtk: connectivity: common: Import common connectivity kernel drivers
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>
    Signed-off-by: dread <dreadcreator5@gmail.com>

--- include/uapi/linux/input-event-codes.h
+++ include/uapi/linux/input-event-codes.h
@@ -278,8 +278,7 @@
 #define KEY_PAUSECD		201
 #define KEY_PROG3		202
 #define KEY_PROG4		203
-#define KEY_ALL_APPLICATIONS	204	/* AC Desktop Show All Applications */
-#define KEY_DASHBOARD		KEY_ALL_APPLICATIONS
+#define KEY_DASHBOARD		204	/* AL Dashboard */
 #define KEY_SUSPEND		205
 #define KEY_CLOSE		206	/* AC Close */
 #define KEY_PLAY		207
@@ -337,6 +336,11 @@
 #define KEY_RFKILL		247	/* Key that controls all radios */
 
 #define KEY_MICMUTE		248	/* Mute / unmute the microphone */
+
+#define KEY_ANT_CONNECT         0x292	/* key for sar ant check */
+#define KEY_ANT_UNCONNECT       0x293	/* key for sar ant check */
+#define DIV_ANT_CONNECT         0X294 	/* key for sar ant div check*/
+#define DIV_ANT_UNCONNECT       0X295 	/* key for sar ant div check*/
 
 /* Code 255 is reserved for special needs of AT keyboard driver */
 
@@ -794,8 +798,7 @@
 #define SW_LINEIN_INSERT	0x0d  /* set = inserted */
 #define SW_MUTE_DEVICE		0x0e  /* set = device disabled */
 #define SW_PEN_INSERTED		0x0f  /* set = pen inserted */
-#define SW_MACHINE_COVER	0x10  /* set = cover closed */
-#define SW_MAX			0x10
+#define SW_MAX			0x0f
 #define SW_CNT			(SW_MAX+1)
 
 /*
